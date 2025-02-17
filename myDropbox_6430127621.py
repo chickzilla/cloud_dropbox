9c98e982f3fd43e1edb14a70622b5b1bad2109ae
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2025-02-17 15:21:56 +0700

    add upload meta data func

--- myDropbox_6430127621.py
+++ myDropbox_6430127621.py
@@ -1,6 +1,5 @@
 import json
 import boto3
-import uuid
 import datetime
 
 s3 = boto3.client("s3")
@@ -8,18 +7,29 @@ s3 = boto3.client("s3")
 S3_BUCKET = "act5-dulyawat"
 DEFAULT_USERNAME = "act4_default_user"
 
+# ========================================================================================================
 def upload_json_to_s3(key, data):
     s3.put_object(Bucket=S3_BUCKET, Key=key, Body=json.dumps(data))
+    
+def upload_metadata(file_name, size, username):
+    metadata_key = f"metadata/{username}/{file_name}.json"
+    metadata = {
+        "file_name": file_name,
+        "last_modified": datetime.datetime.now(datetime.timezone.utc).isoformat(),
+        "owner": DEFAULT_USERNAME,
+        "size": size,
+    }
+    upload_json_to_s3(metadata_key, metadata)
     
 # ========================================================================================================
 def put_file(event):
     file_name = event.get("file_name")
     file_content = event.get("file_content")
 
-    if not file_name or not file_content:
+    if not file_name:
         return {
             "statusCode": 400,
-            "body": json.dumps({"message": "Missing file_name or file_content"})
+            "body": json.dumps({"message": "Missing file_name"})
         }
 
     file_key = f"files/{DEFAULT_USERNAME}/{file_name}"
@@ -28,15 +38,7 @@ def put_file(event):
         # write file
         s3.put_object(Bucket=S3_BUCKET, Key=file_key, Body=file_content)
 
-        # write metadata
-        metadata_key = f"metadata/{DEFAULT_USERNAME}/{file_name}.json"
-        metadata = {
-            "file_name": file_name,
-            "last_modified": datetime.datetime.now(datetime.timezone.utc).isoformat(),
-            "owner": DEFAULT_USERNAME,
-            "size": len(file_content),
-        }
-        upload_json_to_s3(metadata_key, metadata)
+        upload_metadata(file_name, len(file_content), DEFAULT_USERNAME)
 
         return {
             "statusCode": 200,
